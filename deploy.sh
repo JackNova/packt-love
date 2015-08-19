51fc3b2e7f8bb4241bb905edac3e49b4ecd57521
Dario Iacampo <dario@jntstudio.net>
2015-08-19 22:03:29 +0200

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+if [ "$1" == production ]
+then
+	echo 'configuring app for production environment...'
+	app_id=packt-love
+elif [ "$1" == serve ]
+then
+	echo 'configuring app for serve environment...'
+	app_id=packt-love
+else
+	echo 'bad argument. Allowed arguments are [production, staging, serve].'
+	exit 1
+fi
+
+echo 'selected app_id is'
+echo $app_id
+
+# building app.yaml
+cp app.yaml app.yaml.temp 
+sed -e "s/\${app_id}/$app_id/" app.yaml.temp > app.yaml
+
+
+if [[ "$1" == serve  ]]; then
+	dev_appserver.py --port=9988 . --log_level=debug # --clear_datastore=yes
+else
+	echo 'starting deployment'
+	appcfg.py update . --no_cookie
+fi
+
+echo 'deployment complete.'
+echo 'cleaning up...'
+
+rm app.yaml 
+mv app.yaml.temp app.yaml
+
+echo 'complete' 
---
+++ main.py
@@ -0,0 +1,62 @@
+import webapp2
+from helpers import scrape, send_email
+from app_config import config
+import logging
+import urllib2, urllib, cookielib
+
+login_details = {
+	"email": config["PACKT_EMAIL"],
+	"password": config["PACKT_PASSWORD"],
+	"op": "Login",
+	"form_id": "packt_user_login_form",
+	"form_build_id": ""
+}
+
+login_error = 'Sorry, you entered an invalid email address and password combination.'
+
+cj = cookielib.CookieJar()
+http_handler = urllib2.HTTPCookieProcessor(cj)
+opener = urllib2.build_opener(http_handler)
+
+url = 'https://www.packtpub.com/packt/offers/free-learning'
+
+class MainHandler(webapp2.RequestHandler):
+	def get(self):
+		# scrape essential informations
+		get_book_url, book_title, new_form_id, image_url, book_description = scrape(url,
+			"//a[contains(@class, 'twelve-days-claim')]/@href",
+			"//div[contains(@class, 'dotd-title')]",
+			"//input[@type='hidden'][starts-with(@id, 'form')][starts-with(@value, 'form')]/@value",
+			"//img[contains(@class, 'bookimage')]/@src",
+			"//*[@id='deal-of-the-day']/div/div/div[2]/div[3]")
+
+
+		if new_form_id:
+			login_details["form_build_id"] = new_form_id
+
+		logging.info('get_book_url: %s \n book_title: %s \n new_form_id: %s \n image_url: %s' % (get_book_url, book_title, new_form_id, image_url))
+		logging.info('book_description: \n %s' % book_description)
+
+		# login
+		login_payload = urllib.urlencode(login_details)
+		login_request = urllib2.Request(url, login_payload, {'content-type': 'application/x-www-form-urlencoded'})
+		login_response = opener.open(login_request)
+		login_failed = login_error in login_response.read()
+
+		if login_failed:
+			logging.error('login failed')
+			self.error(401)
+			return
+
+		# grab book
+		grab_book = opener.open('https://www.packtpub.com' + get_book_url)
+		grab_book_response = grab_book.read()
+
+		send_email(book_title, book_description, 'https:'+image_url)
+
+		self.response.write('done')
+
+
+app = webapp2.WSGIApplication([
+	('/', MainHandler)
+], debug=True)
