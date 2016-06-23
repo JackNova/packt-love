e785ea67edef10d29e587a51dee0a5bb6ba51df1
Dario Iacampo <dario@jntstudio.net>
2016-06-23 18:39:15 +0200

    fetch my ebooks ids

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import webapp2
-from model import scrape, send_email, curl
+from model import scrape, send_email, curl, scrape_many
 from app_config import config
 import logging
 import urllib2, urllib, cookielib
@@ -24,6 +24,43 @@ opener = urllib2.build_opener(http_handler)
 
 url = 'https://www.packtpub.com/packt/offers/free-learning'
 
+def login():
+
+	cj = cookielib.CookieJar()
+	http_handler = urllib2.HTTPCookieProcessor(cj)
+	opener = urllib2.build_opener(http_handler)
+
+	# just need the new_form_id for logging in
+	raw_html = curl(url)
+	new_form_id, = scrape(raw_html,
+		"//input[@type='hidden'][starts-with(@id, 'form')][starts-with(@value, 'form')]/@value")
+
+	login_details = {
+		"email": config["PACKT_EMAIL"],
+		"password": config["PACKT_PASSWORD"],
+		"op": "Login",
+		"form_id": "packt_user_login_form",
+		"form_build_id": ""
+	}
+
+	if new_form_id:
+		login_details["form_build_id"] = new_form_id
+
+	# login
+	login_payload = urllib.urlencode(login_details)
+	login_request = urllib2.Request(url, login_payload, {'content-type': 'application/x-www-form-urlencoded'})
+	login_response = opener.open(login_request, timeout=45)
+	login_failed = login_error in login_response.read()
+
+	if login_failed:
+		logging.error('login failed')
+		self.error(401)
+		return
+
+	logging.info("login succeded")
+
+	return opener
+
 class PurchaseFreeEbookTask(webapp2.RequestHandler):
 	def post(self):
 		# scrape essential informations
@@ -34,7 +71,6 @@ class PurchaseFreeEbookTask(webapp2.RequestHandler):
 			"//input[@type='hidden'][starts-with(@id, 'form')][starts-with(@value, 'form')]/@value",
 			"//img[contains(@class, 'bookimage')]/@src",
 			"//*[@id='deal-of-the-day']/div/div/div[2]/div[3]")
-
 
 		if new_form_id:
 			login_details["form_build_id"] = new_form_id
@@ -66,8 +102,30 @@ class SchedulePurchaseFreeEBook(webapp2.RequestHandler):
 	def get(self):
 		taskqueue.add(url='/task/purchase-free-ebook')
 
+class DownloadPurchasedEBooksTask(webapp2.RequestHandler):
+	def post(self):
+		opener = login()
+
+		# get all my purchased ebooks ids
+		my_ebooks = opener.open('https://www.packtpub.com/account/my-ebooks', timeout=45)
+		my_ebooks_response = my_ebooks.read()
+		ebooks_ids, = scrape_many(my_ebooks_response, "//div[contains(@class, 'product-line')]/@nid")
+		logging.info('my ebooks are following: %s' % ebooks_ids)
+		self.response.write(ebooks_ids)
+
+		# for each ebook id schedule download of all resources
+		
+
+		self.response.write('done')
+
+class ScheduleDownloadPurchasedEBooks(webapp2.RequestHandler):
+	def get(self):
+		taskqueue.add(url='/task/download-purchased-ebooks')
+		
 
 app = webapp2.WSGIApplication([
 	('/schedule-purchase-free-ebook', SchedulePurchaseFreeEBook),
-	('/task/purchase-free-ebook', PurchaseFreeEbookTask)
+	('/task/purchase-free-ebook', PurchaseFreeEbookTask),
+	('/schedule-download-purchased-ebooks', ScheduleDownloadPurchasedEBooks),
+	('/task/download-purchased-ebooks', DownloadPurchasedEBooksTask)
 ], debug=True)
